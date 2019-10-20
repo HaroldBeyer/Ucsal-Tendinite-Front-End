85814cbe2ddea5351dcecf6945b75994584aab48
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-20 11:18:50 -0300

    Refactoring code

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -1,21 +1,8 @@
 // Create a WorldWindow for the canvas.
 var wwd = new WorldWind.WorldWindow("canvasOne");
-/*
-const Http = new XMLHttpRequest();
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange = e => {
-    console.log(Http.responseText);
-};
-*/
-//getDiseases();
-
-//const http = require("http");
-//http.get("https//localhost:3000/diseases", res => {
-// console.log(res);
-//});
 console.log("It works!");
 
+//Adding layers
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 
@@ -25,71 +12,25 @@ wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
 // Add a placemark
 createPlacemark();
-//addPlacemark("Teste", -12.56962, -38.533074);
-
-var logError = function(jqXhr, text, exception) {
-  console.log(
-    "There was a failure retrieving the capabilities document: " +
-      text +
-      " exception: " +
-      exception
-  );
-};
-
-$.get(serviceAddress)
-  .done(createLayer)
-  .fail(logError);
 
 /**
- * Get the diseases (lmao that's not a good name, but it's the object that comes from our api)
- * Plz nasa approve my project, i love you <3
+ * Create placemarks automatically from our local loopback api.
  */
-function getDiseases() {
-  const Http = new XMLHttpRequest();
-  const url = "http://localhost:3000/api/diseases";
-  Http.open("GET", url);
-  Http.send();
-  const obj = [];
-  let done = false;
-  Http.onreadystatechange = e => {
-    while (!done) {
-      const dados = JSON.parse(e["currentTarget"]["response"]);
-      if (dados && dados != "") {
-        //  console.log("Arquivo total:" + dados);
-        const keys = Object.keys(dados);
-        for (const key of keys) {
-          obj.push(dados[key]);
-        }
-        // console.log("Dados recebidos: " + obj);
-        return obj;
-        done = true;
-      } else {
-        //Treating errors;
-        done = false;
-      }
-    }
-  };
-}
-
 function createPlacemark() {
   const placemarkLayer = new WorldWind.RenderableLayer();
   wwd.addLayer(placemarkLayer);
   const placemarkAttributes = genericPlaceMarkAttributes();
-  //  const obj = getDiseases();
+  //Get the data from the api
   request("GET", "http://localhost:3000/api/diseases")
     .then(res => {
-      // console.log("Promise is strong", res);
       const obj = [];
       const dados = JSON.parse(res["currentTarget"]["response"]);
       if (dados && dados != "" && !dados["error"]) {
-        // console.log("Arquivo total:" + dados);
         const keys = Object.keys(dados);
         for (const key of keys) {
           obj.push(dados[key]);
         }
-        // console.log("Dados recebidos: " + obj);
         for (const disease of Object.keys(obj)) {
-          //  console.log("Resultado: " + disease);
           const countries = Object.keys(obj[disease]["countries"]);
           for (const country of countries) {
             const name = obj[disease]["name"];
@@ -99,6 +40,7 @@ function createPlacemark() {
               obj[disease]["countries"][country]["ltn"] ||
               obj[disease]["countries"][country]["ltn"];
             const plac3mark = addPlacemark(name, lat, ltn, countryName);
+            //add placemark to the map
             placemarkLayer.addRenderable(plac3mark);
           }
         }
@@ -107,9 +49,10 @@ function createPlacemark() {
     .catch(err => {
       // console.error(new Error(err));
     });
-  return position;
 }
-
+/**
+ * Create a generic placeMarkAttribute.
+ */
 function genericPlaceMarkAttributes() {
   const placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
   placemarkAttributes.imageOffset = new WorldWind.Offset(
@@ -128,10 +71,14 @@ function genericPlaceMarkAttributes() {
     WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
   return placemarkAttributes;
 }
+/**
+ * Add the placemark with the followings params
+ * @param {string} nome is the name of the disease
+ * @param {number} lat is the latitude of the disease
+ * @param {number} lng is the longitude of the disease
+ * @param {string} countryName is the name of the country in which the disease has occurred;
+ */
 function addPlacemark(nome, lat, lng, countryName) {
-  // console.log(
-  //  "Adicionando placemark. Nome: " + nome + "lat e ltn:" + lat + " " + lng
-  //);
   const posicao = new WorldWind.Position(lat, lng, 100.0);
   const placeMarkAttribute = genericPlaceMarkAttributes();
   const placemarkn = new WorldWind.Placemark(
@@ -153,7 +100,11 @@ function addPlacemark(nome, lat, lng, countryName) {
   placemarkn.alwaysOnTop = true;
   return placemarkn;
 }
-
+/**
+ * Simple HTTP request, with the followings params:
+ * @param {string} method  for the method type
+ * @param {string} url  for the api's url
+ */
 function request(method, url) {
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
